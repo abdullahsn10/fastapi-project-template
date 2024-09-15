7d837206fc7ad94c924a8633fa33281715b93d9a
abdullahsn10 <1201952@student.birzeit.edu>
2024-09-15 18:26:39 +0300

    Add the project setup

---
+++ src/schemas/__init__.py
@@ -0,0 +1,6 @@
+from src.schemas.authentication import *
+from src.schemas.branch import *
+from src.schemas.coffee_shop import *
+from src.schemas.customer import *
+from src.schemas.user import *
+from src.schemas.report import *
