c968d47a7170e4881bcb578a70104b3e0d1303fb
abdullahsn10 <1201952@student.birzeit.edu>
2024-09-15 18:35:31 +0300

    Add readme file

--- src/schemas/__init__.py
+++
@@ -1,6 +0,0 @@
-from src.schemas.authentication import *
-from src.schemas.branch import *
-from src.schemas.coffee_shop import *
-from src.schemas.customer import *
-from src.schemas.user import *
-from src.schemas.report import *
